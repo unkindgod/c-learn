ff4ea7387de6ecd3b70a1d04ca68b687e08dc066
Bulygin Sergei <s.bulygin@necneva.local>
2019-07-15 22:00:57 +0300

    Initial commit

---
+++ l 0-1/class/sayhello/sayhello/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+
+
+namespace sayhello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            Console.WriteLine("Введите имя: ");
+            string name = Console.ReadLine();
+            Thread.Sleep(5000);
+            Console.WriteLine($"Здравствуйте, {name}!");
+            Console.ReadKey();
+        }
+    }
+}
