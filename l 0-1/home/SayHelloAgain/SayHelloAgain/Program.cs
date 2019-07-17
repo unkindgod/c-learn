5784edfe458aa836ce49ef2835f8c3b7c94efc59
Bulygin Sergei <s.bulygin@necneva.local>
2019-07-17 15:38:44 +0300

    HomeWork

---
+++ l 0-1/home/SayHelloAgain/SayHelloAgain/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+
+
+namespace sayhelloagain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            Console.WriteLine("Введите имя: ");
+            string name = Console.ReadLine();
+            Thread.Sleep(5000);
+            Console.WriteLine($"Здравствуйте, {name}!");
+            Thread.Sleep(5000);
+            Console.WriteLine($"До свидания, {name}!");
+            Console.ReadKey();
+        }
+    }
+}
