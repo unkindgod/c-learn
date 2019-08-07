ca621ff72b6e39c04c1eb0217763d0dbd9f5afd1
deckardhere@gmail.com <deckardhere@gmail.com>
2019-08-07 22:02:30 +0300

    ClassWork-4

---
+++ l_0-4/class/cw_4-2/cw-4-4/cw-4-4/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace cw_4_4
+{
+    class Program
+    {
+        enum WindDirection
+        {
+            None = 0,
+            North = 1,
+            East = 1 << 1,
+            South = 1 << 2,
+            West = 1 << 3
+        }
+        static void Main(string[] args)
+        {
+            int a = 3;
+            int b = int.MaxValue;
+
+            //int c = a | b;
+
+            Console.WriteLine(ToBynaryString(a));
+            Console.WriteLine(ToBynaryString(a | b));
+            Console.WriteLine(ToBynaryString(a & b));
+            Console.WriteLine(ToBynaryString(a ^ b));
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+            int shiftSample = 7;
+            Console.WriteLine(ToBynaryString(shiftSample));
+            Console.WriteLine(ToBynaryString(shiftSample << 1));
+            Console.WriteLine(ToBynaryString(shiftSample << 2));
+            Console.WriteLine(ToBynaryString(shiftSample << 7));
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine(ToBynaryString((int)WindDirection.North));
+            Console.WriteLine(ToBynaryString((int)WindDirection.East));
+            Console.WriteLine(ToBynaryString((int)WindDirection.South));
+            Console.WriteLine(ToBynaryString((int)WindDirection.West));
+
+            WindDirection wd = WindDirection.East | WindDirection.North;
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine(ToBynaryString((int)wd));
+
+            Console.WriteLine(string.Join(" \t ", new[] { 1, 2, 4, 6}));
+        }
+        static string ToBynaryString(int a)
+        {
+            return Convert.ToString(a, 2).PadLeft(32, '0') + $" ({a})";
+        }
+    }
+}
---
+++ l_0-4/class/cw_4-2/cw_4-3/Classwork-4-3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Classwork_4_3
+{
+    class Program
+    {
+        enum Day
+        {
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday
+        };
+        enum Month : byte
+        {
+            Jan,
+            Feb,
+            Mar,
+            Apr,
+            May,
+            Jun,
+            Jul,
+            Aug,
+            Sep,
+            Oct,
+            Nov,
+            Dec
+        };
+        static void Main()
+        {
+          Day today = Day.Monday;
+            int dayNumber = (int)today;
+            Console.WriteLine("{0} is a day number #{1}", today, dayNumber);
+        }
+    }
+}
