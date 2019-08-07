14e983480184704830aed873eb8e9f51919b3e83
deckardhere@gmail.com <deckardhere@gmail.com>
2019-08-07 16:42:40 +0300

    HomeWork-2

---
+++ l 0-2/Homework/HomeWork-2/HomeWork-2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+
+namespace HomeWork_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            Console.WriteLine("Введите первое число:");
+            var a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число:");
+            var b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Какую операцию необходимо выполнить (+, -, *, /)?:");
+            var c = char.Parse(Console.ReadLine());
+            double total;
+
+            if (c == '+')
+            {
+                total = a + b;
+                Console.WriteLine($"Сумма равна: {total}");
+            }
+            else if (c == '-')
+            {
+                total = a - b;
+                Console.WriteLine($"Разность равна: {total}");
+            }
+            else if (c == '*')
+            {
+                total = a * b;
+                Console.WriteLine($"Умножение равно: {total}");
+            }
+            else if (c == '/')
+            {
+                total = a / b;
+                Console.WriteLine($"Деление равно: {total}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
