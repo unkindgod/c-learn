9323f4c6fb0a1446f786b0b896233245816fd1ec
deckardhere@gmail.com <deckardhere@gmail.com>
2019-08-07 20:35:17 +0300

    ClassWork4-1

---
+++ l_0-4/class/cw_4-1/classwork_4-1/classwork_4-1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace classwork_4_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a = 18;
+            int b = a++;
+
+            Console.WriteLine(a == b);
+            Console.WriteLine(a != b);
+            Console.WriteLine(a > b);
+            Console.WriteLine(a < b);
+            Console.WriteLine(a <= b);
+            Console.WriteLine(a >= b);
+        }
+    }
+}
---
+++ l_0-4/class/cw_4-2/classwork_4-2/classwork_4-2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace classwork_4_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            Console.WriteLine("Укажите переменную a:");
+            var a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Укажите переменную h:");
+            var h = double.Parse(Console.ReadLine());
+
+            double s1 = 3 * a * h;
+            Console.WriteLine(s1);
+            double s2 = 3 / 2 * a * (a * Math.Sqrt(3) + 2 * h);
+            Console.WriteLine(s2);
+
+            Console.ReadKey();
+        }
+    }
+}
