46cd5ce221ded3f96a449e165dc985fd484e0245
deckardhere@gmail.com <deckardhere@gmail.com>
2019-08-07 17:32:55 +0300

    HomeWork-3

---
+++ l 0-3/home/HomeWork-3/HomeWork-3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+namespace HomeWork_3
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] x = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            int[] y = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+            for (int i = 1; i < 11; i++)
+            {
+                for (int o = 1; o < 11; o++)
+                {
+                    Console.WriteLine(i * o);
+                }
+            }
+            Console.ReadKey();    
+        }
+    }
+}
